24e5bc02ce0ea927d86bf73f4a530b4cad1d6b35
saboorab <96356526+saboorab@users.noreply.github.com>
2022-01-05 02:10:24 +0500

    incorporating comments

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -1,3 +1,6 @@
+//Defining the URL configurable such as Base_URI so that test can be executed in multiple environments only changing the BASE_URI
 export const BASE_URI = "https://api-demo.airwallex.com";
+//Defining the AUTH_LOGIN endpoints
 export const AUTH_LOGIN = '/api/v1/authentication/login';
+//Defining the create beneficiary endpoints
 export const CREATE_BENEFICIARIES = '/api/v1/beneficiaries/create';
