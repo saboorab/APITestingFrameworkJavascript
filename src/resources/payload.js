aa9ec63b4f25cedb13754fa57b3198078f9b3127
saboorab <96356526+saboorab@users.noreply.github.com>
2022-01-05 02:00:49 +0500

    incorporating comments

--- src/resources/payload.js
+++ src/resources/payload.js
@@ -1,3 +1,4 @@
+//Creating payload for createbeneficiary post rquest
 export const createBeneficiariesPayload = (paymentMethod, bankCountryCode, accountName, accountNumber, swiftCode, bsb, aba) => {
     var payload = {
         bank_country_code: bankCountryCode,
@@ -7,6 +8,7 @@ export const createBeneficiariesPayload = (paymentMethod, bankCountryCode, accou
 
     }
 
+ //creating generic request bddy
     const requestBody = {
         "beneficiary": {
             "additional_info": {
@@ -31,32 +33,40 @@ export const createBeneficiariesPayload = (paymentMethod, bankCountryCode, accou
        
     };
 
+//Defining payment method,either proper value can be supplied from the values function in utils->random or it can be empty    
     if( (paymentMethod || paymentMethod  === '')){
         requestBody["payment_methods"]= [paymentMethod ] 
 }
-//console.log(requestBody.bank_details,benficary.bankCountryCode)
+//Defining bankCountryCode,either proper value can be supplied from the values function in utils->random or it can be empty
     if( (bankCountryCode || bankCountryCode  === '')){
          requestBody.beneficiary.bank_details["bank_country_code"]= bankCountryCode 
 }
-
+//Defining accountName,either proper value can be supplied from the accountname function in utils->random or it can be empty
     if( (accountName || accountName  === '')){
         requestBody.beneficiary.bank_details["account_name"]= accountName 
 }
+//Defining bankCountryCode,either proper value can be supplied from the account number function in utils->random or it can be empty
     if( (accountNumber || accountNumber  === '')){
         requestBody.beneficiary.bank_details["account_number"]= accountNumber
 }
 
+//Adding business_registration_number in the request in case of bank_country_code === 'CN' as it is mandatory
     if (payload.bank_country_code === 'CN') {
         requestBody.beneficiary.additional_info['business_registration_number'] = 'IT593003';
     }
+
+//Adding bsb tag and value from the values funtion in utils->random.js in the request or it can have empty values
     if (bsb || bsb === '' ) {
         requestBody.beneficiary.bank_details['account_routing_type1'] = 'bsb';
         requestBody.beneficiary.bank_details['account_routing_value1'] = bsb;
-    } else if (aba || aba === '') {
+    } 
+//Adding aba tag and value from the values funtion in utils->random.js in the request or it can have empty values
+    else if (aba || aba === '') {
         requestBody.beneficiary.bank_details['account_routing_type1'] = 'aba';
         requestBody.beneficiary.bank_details['account_routing_value1'] = aba;
     }
 
+//Adding swift tag and value from the values funtion in utils->random.js in the request or it can have empty values
     if (swiftCode || swiftCode === '') {
         requestBody.beneficiary.bank_details['swift_code'] = swiftCode;
     }
